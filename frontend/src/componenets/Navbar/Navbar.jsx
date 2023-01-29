617e2acd859f4706290869be860cece6fc42899c
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-29 18:33:33 +0300

    complate header

--- frontend/src/componenets/Navbar/Navbar.jsx
+++ frontend/src/componenets/Navbar/Navbar.jsx
@@ -13,47 +13,49 @@ const Navbar = () => {
     setToggle((prevState) => !prevState);
   };
   return (
-    <na className="app__navbar">
-      <div className='app__navbar-logo'>
-        <img src={images.logo} alt="logo" />
-      </div>
-        <ul className='app__navbar-links'>
-          {['home','about','work','skills','contact'].map((item)=>(
-            <li className='app__flex p-tex'  key={`link-${item}`}>
-              <a href={`#${item}`}><span>{'<'} </span>{item}<span> {'/ >'}</span></a>
-              <div />
-            </li>
-          ))}
-        </ul>
-
-        <div className='app__navbar-menu'>
-            <HiMenuAlt4 onClick={()=> setToggle(true)} />
-            {
-              toggle && (
-                <motion.div
-                initial={{ width: 0 }}
-                animate={{ width: 300 }}
-                transition={{duration:0.85,ease:'easeOut'}}
-                >
-            <motion.span
-              initial={{ width: 0 }}
-              animate={{ width: 70 }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
-              <HiX onClick={(e) => handleOnClick(e)} />
-              </motion.span>
-                  <ul>
-                  {['home','about','work','skills','contact'].map((item)=>(
-                    <li key={item}>
-                      <a href={`#${item}`} onClick={() => setToggle(false)}><span>{'<'} </span>{item}<span> {'/ >'}</span></a>
-                    </li>
-                  ))}
-                </ul>
-                </motion.div>
-              )
-            }
+    <nav className="app__navbar">
+      <div className='app__navbar-container'>
+        <div className='app__navbar-logo'>
+          <img src={images.logo} alt="logo" />
         </div>
-    </na>
+          <ul className='app__navbar-links'>
+            {['home','about','work','skills','contact'].map((item)=>(
+              <li className='app__flex p-tex'  key={`link-${item}`}>
+                <a href={`#${item}`}><span>{'<'} </span>{item}<span> {'/ >'}</span></a>
+                <div />
+              </li>
+            ))}
+          </ul>
+
+          <div className='app__navbar-menu'>
+              <HiMenuAlt4 onClick={()=> setToggle(true)} />
+              {
+                toggle && (
+                  <motion.div
+                  initial={{ width: 0 }}
+                  animate={{ width: 300 }}
+                  transition={{duration:0.85,ease:'easeOut'}}
+                  >
+              <motion.span
+                initial={{ width: 0 }}
+                animate={{ width: 70 }}
+                transition={{ duration: 0.85, ease: 'easeOut' }}
+              >
+                <HiX onClick={(e) => handleOnClick(e)} />
+                </motion.span>
+                    <ul>
+                    {['home','about','work','skills','contact'].map((item)=>(
+                      <li key={item}>
+                        <a href={`#${item}`} onClick={() => setToggle(false)}><span>{'<'} </span>{item}<span> {'/ >'}</span></a>
+                      </li>
+                    ))}
+                  </ul>
+                  </motion.div>
+                )
+              }
+          </div>
+      </div>
+    </nav>
   )
 }
 
