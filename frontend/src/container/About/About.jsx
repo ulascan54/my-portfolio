ec655747b9f92b8298e6ec969a85e6bbd45163e8
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-30 21:01:21 +0300

    motionwrap created

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,7 +1,8 @@
-import React,{ useState, useEffect } from 'react'
-import { motion } from 'framer-motion'
-import './About.scss'
-import { AppWrap } from '../../wrapper/index';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
 import { urlFor, client } from '../../client';
 
 const About = () => {
@@ -14,30 +15,32 @@ const About = () => {
       setAbouts(data);
     });
   }, []);
+
   return (
     <>
-      <h2 className='head-text'>
-       I Know That <span>Good Apps</span> 
-        <br/> means <span>Good Business</span>
-      </h2>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
-        {abouts.map((about,index)=>(
+        {abouts.map((about, index) => (
           <motion.div
-           whileInView={{opacity:1}}
-           whileHover={{scale:1.1}}
-           transition={{duration:0.5, type:'tween'}}
-           className="app__profiles-item"
-           key={about.title+index}
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
           >
             <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
-            <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AppWrap(About, 'about') 
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
